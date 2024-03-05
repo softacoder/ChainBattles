ee2cb4d9b3cc5d7db721230304eb5ba6f6976318
softacoder <113185323+softacoder@users.noreply.github.com>
2024-03-05 15:41:20 +0000

    commit chainbattles.sol and deploy.js

--- contracts/ChainBattles.sol
+++ contracts/ChainBattles.sol
@@ -6,15 +6,77 @@ import "openzeppelin/contracts/utils/Counters.sol";
 import "openzeppelin/contracts/utils/Strings.sol";
 import "openzeppelin/contracts/utils/Base64.sol";
 
-contact ChainBattles is ERC721URIStorage {
+contract ChainBattles is ERC721URIStorage {
     using Strings for unit256;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     mapping(uint256 => uint256) public tokenIdtoLevels;
 
+// extra
+// track levels
+// track hp
+// track strength
+// track speed
+// figure out how to do sudo number generation onchains
+
     constructor() ERC721("Chain Battles", "CBTLS") {
 
     }
+
+    function generateCharacter(uint256 tokenId) public returns (string memory){
+        bytes memory svg = abi.encodePacked(
+            '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
+            '<styles>.base { fill: white; font-family: serif; font-size: 14px; }</styles>,
+            'rect width="100%" height="100%" fill="black" />',
+            '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle"'>, "Warrior", '</text>'
+            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle"'>, "Levels: ".getLevels(tokenId), '</text>',
+            '</svg>'
+        );
+
+        return string(
+            abi.encodePacked(
+                "data:image/svg-xml:base64,",
+                Base.encode(svg)
+            );
+        )
+    }
+
+    function getLevels(uint256 tokenId) public view return(string memory){
+        uint256 levels = tokenIdToLevels[tokenID]
+        return levels.toString()
+    }
+
+    function getTokenURI(uint256 tokenId) public returns (string memory){
+        bytes memory dataURI = abi.encodePacked(
+            '{',
+            '"name": "Chain Battles #", tokenId.toString(), '",',
+            '"description": "Battles on chain",',
+            '"image": "', generateCharacter(tokenId), '"',
+            '}'
+        );
+       
+        return string(
+            abi.encodePacked(
+                "data:application/json:base64,",
+                Base64.encode(dataURI)
+            )
+        );
+    }
+
+    function mint() public {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _safemint(msg.sender, newItemId);
+        tokenIdtoLevels[newItemId] = 0;
+        _setTokenURI(newItemId, getTokenURI(newItemId))
+    }
+    function train(uint tokenId) public {
+        require(exists(tokenId), "Please use an existing Token");
+        require(ownerOf(tokenId) == msg.sender, "You must own this token train it");
+        uint256 currentLevel = tokenIdtoLevels[tokenId];
+        tokenIdtoLevels[tokenId] = currentLevel -1;
+        _setTokenURI(tokenId, getTokenURI(tokenId));
 }
-// start video at 16:30
+}
+// start video at 39 min
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,22 @@
+const main = async () => {
+  try {
+    const nftContractFactory = await hre.ethers.getContractFactory(
+      "ChainBattles"
+    );
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.deployed();
+
+    console.log("Contract deployed to:", nftContract.address);
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+main();
+
+// below is code that came after installing the packages. Should i delete? above is code from video.
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
